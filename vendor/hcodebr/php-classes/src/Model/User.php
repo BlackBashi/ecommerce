368d5dca55930cb847e8b4cac7f824a6447406c5
BlackBashi <arlindomauricio2013sk8@gmail.com>
2019-08-13 15:35:42 -0400

    Site - Cadastro

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -362,10 +362,27 @@ class User extends Model {
         $_SESSION[User::ERROR] = NULL;
     }
 
-    public function setErrorRegister($msg)
-    {
+    public function setErrorRegister($msg){
+    
        $_SESSION[User::ERROR_REGISTER] = $msg;
     }
+
+    public static function getErrorRegister(){
+        $msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+        User::clearErrorRegister();
+        return $msg;
+    }
+
+    public static function clearErrorRegister(){
+        $_SESSION[User::ERROR_REGISTER] = NULL;
+    }
     
+    public static function checkLoginExist($login){
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+            ':deslogin'=>$login
+        ]);
+        return (count($results) > 0);
+    }
 
 }
